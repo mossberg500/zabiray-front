559325578379450b1fa532b27028496a2631aebd
mossberg500 <axaptasub@gmail.com>
2021-11-10 18:40:34 +0200

    верстка 10112021_10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from './views/Header';
+import Technologies from './views/Technologies';
+import Footer from "./views/Footer";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://st2.depositphotos.com/1064024/10769/i/600/depositphotos_107694484-stock-photo-little-boy.jpg'/>
+                <Header/>
+            </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Main content</div>
+            </nav>
+
+                <div className='content'>
+                    <div>
+                        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQIeCxX7mFVg3phbJYG5Ho6dR_apaGg5wjbg&usqp=CAU' />
+                    </div>
+                    <div>
+                        ava + description
+                    </div>
+                    <div>
+                        My Posts
+                        <div>
+                            New post
+                        </div>
+                        <div>
+                            <div>post 1</div>
+                            <div>post 2</div>
+                            <div>post 3</div>
+                        </div>
+                    </div>
+                    <Technologies/>
+                </div>
+
+            <footer className='footer'><Footer/></footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/views/Technologies.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Technologies = () => {
+    return (
+        <div>
+            <div>aa</div>
+            <div>bb</div>
+            <div>cc</div>
+        </div>
+    )
+}
+export default Technologies;
